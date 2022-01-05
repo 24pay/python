7f179215cfff6e62aafb165424e54d190f88c421
24pay <durcak@24-pay.eu>
2022-01-05 23:01:57 +0100

    Update example.py

--- example.py
+++ example.py
@@ -3,4 +3,4 @@ from Signer24pay import Signer24pay
 signer = Signer24pay("demoOMED", "1234567812345678123456781234567812345678123456781234567812345678")
 
 print(signer.sign("Compute Test Sign").decode("utf8"))
-# with default parameters will print out 0C4F8AAEE7729C869653CF3BB3F4A621
+# with default params will print out 0C4F8AAEE7729C869653CF3BB3F4A621
