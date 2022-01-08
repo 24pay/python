50411343c69d7017fdb88901ab22f8e3f5dcdfc6
24pay <durcak@24-pay.eu>
2022-01-08 23:34:15 +0100

    Update Signer24pay.py
    Proper pkcs7 padding

--- Signer24pay.py
+++ Signer24pay.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import AES
 import binascii
 import hashlib
+from cryptography.hazmat.primitives import padding
 
 class Signer24pay:
 
@@ -8,11 +9,13 @@ class Signer24pay:
         self.iv = mid + mid[::-1]
         self.key = binascii.unhexlify(key)
 	
-    def __pad(self, s):
-        return s + b" " * (AES.block_size - len(s) % AES.block_size)
+#    def __pad(self, s):
+#        return s + b" " * (AES.block_size - len(s) % AES.block_size)
 
     def sign(self, plaintext):
         message = hashlib.sha1(plaintext.encode('utf-8')).digest()
-        message = self.__pad(message)
+#       message = self.__pad(message)
+        padder = padding.PKCS7(128).padder()
+        message = padder.update(message) + padder.finalize()
         cipher = AES.new(self.key, AES.MODE_CBC, self.iv.encode("utf8"))
         return binascii.hexlify(cipher.encrypt(message))[:32].upper()
